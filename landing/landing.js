1304eb372e8d04951a54cf661ed231412640b3ef
Abhinav-Shrivas <abhinavshrivas2401@gmail.com>
2026-03-11 11:29:45 +0530

    context menu removed

--- landing/landing.js
+++ landing/landing.js
@@ -1,14 +1,14 @@
-// document.addEventListener("contextmenu", e => e.preventDefault());
+document.addEventListener("contextmenu", e => e.preventDefault());
 
-// document.addEventListener("keydown", e => {
-//   if (
-//     e.key === "F12" ||
-//     (e.ctrlKey && e.shiftKey && ["I", "J", "C"].includes(e.key)) ||
-//     (e.ctrlKey && e.key === "U")
-//   ) {
-//     e.preventDefault();
-//   }
-// });
+document.addEventListener("keydown", e => {
+  if (
+    e.key === "F12" ||
+    (e.ctrlKey && e.shiftKey && ["I", "J", "C"].includes(e.key)) ||
+    (e.ctrlKey && e.key === "U")
+  ) {
+    e.preventDefault();
+  }
+});
 
 
 let lastScrollY = window.scrollY;
